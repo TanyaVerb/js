0f3d37a522046198fa3dae71ac6c21a44adbe389
Tanya <test@mail.ru>
2024-02-26 21:18:08 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+// console.log('Hello world1');
+// console.log('Hello world2');
+// console.log('11111');
+// console.log(555);
+// console.log(2 * 3);
+// console.log();
+//  let person = 'Tanya';
+// console.log(person);
+
+ let person = 'Tanya';
+ let car = 'BMW';
+ let job = 'FE';
+
+//  let person = 'Tanya',
+//   car = 'BMW',
+//   job = 'FE',
+
+/* Comments Comments Comments */
+
+
+const LOGIN = 'My Login'
+const PASSWORD = 'My password'
+
+// var LOGIN = 'My Login'
+// var PASSWORD = 'My password'
+
+
+
+
+
+// console.log(person);
+// console.log(car);
+// console.log(job);
+
+let name = 'Tanya';
+ let age = 18;
+ let salary = 5000;
+
+ console.log(person);
+console.log(age);
+console.log(salary);
+
+let dream = 500000000;
+console.log(dream);
+let year = 12;
+console.log(year);
+
+
+
+console.log(salary * year);
+
+console.log(dream/salary);
+
+
