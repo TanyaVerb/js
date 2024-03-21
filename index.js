58d8e1cb5dda582cff283f0022045b8c10490178
TanyaVerb <karra_t@mail.ru>
2024-03-21 14:04:08 +0300

    function

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 // let userOld = prompt('Введите Ваш возраст');
 
-
-
 // if(userOld>=18){
 //     confirm('Ваш возраст подтвержден')
 // }else{
-//     confirm('Ваш возраст не подтвержден') 
+//     confirm('Ваш возраст не подтвержден')
 // }
 
-// userOld>=18 ? confirm('Ваш возраст подтвержден'):confirm('Ваш возраст не подтвержден') 
+// userOld>=18 ? confirm('Ваш возраст подтвержден'):confirm('Ваш возраст не подтвержден')
 
-// userOld>=18 && userOld <40 
+// userOld>=18 && userOld <40
 //  ? confirm('Ваш возраст подтвержден')
 //  :userOld >=40?
 //   confirm('Ваш возраст 40!') :
-//   confirm(`Вы ввели ${userOld}`) 
-
+//   confirm(`Вы ввели ${userOld}`)
 
 // userOld >=0 && userOld <18
 // ? confirm('Ваш возраст подтвержден')
-//  :userOld >=30 && userOld ==39 
-//   ? confirm('Ваш возраст 30-39') 
+//  :userOld >=30 && userOld ==39
+//   ? confirm('Ваш возраст 30-39')
 //   :userOld >40?
-//   confirm('Ваш возраст 40') 
+//   confirm('Ваш возраст 40')
 //   :userOld ==NaN ?
-//   confirm(`Вы ввели ${userOld} !`) : 
+//   confirm(`Вы ввели ${userOld} !`) :
 //   console.log(1)
-
 
 //   userOld <=18 ? confirm('Ваш возраст подтвержден'):
 //   userOld >= 30 && userOld <= 39?
@@ -47,39 +43,34 @@
 // 'use strict'
 
 //   let userName = prompt('ваше имя')
-//   userName ||=  'Вы не ввели имя'  // если вернется пустая строка 
+//   userName ||=  'Вы не ввели имя'  // если вернется пустая строка
 
 //   confirm( `Привет ${userName}`)
 
-  //  userName &&= `Привет ${userName}`
+//  userName &&= `Привет ${userName}`
 
+// let balance = prompt(`Ваш баланс составляет? `)
 
+// Number (balance) >= 10000?
+// console.log (`Ваш баланс составляет ${balance} и вы можете совершить покупку`):
+// console.log(`Нужно еще поработать`)
 
-  // let balance = prompt(`Ваш баланс составляет? `)
+// =================Задача 1=======================================
 
-  // Number (balance) >= 10000?
-  // console.log (`Ваш баланс составляет ${balance} и вы можете совершить покупку`):
-  // console.log(`Нужно еще поработать`)
+// let balance = prompt(`Ваш баланс составляет? `)
 
-  // =================Задача 1=======================================
-
-  // let balance = prompt(`Ваш баланс составляет? `)
- 
-  //   if (typeof +balance === 'number'&& +balance >= 10000) {
-  //   console.log (`Ваш баланс составляет ${balance} и вы можете совершить покупку`)
-  // } else if (typeof +balance === 'number'&& +balance <= 10000) {
-  //   console.log(`Нужно еще поработать`)
-  // } else {
-  //   console.log(`Вы ввели не числовое значение попробуй еще раз`)
-  // }
+//   if (typeof +balance === 'number'&& +balance >= 10000) {
+//   console.log (`Ваш баланс составляет ${balance} и вы можете совершить покупку`)
+// } else if (typeof +balance === 'number'&& +balance <= 10000) {
+//   console.log(`Нужно еще поработать`)
+// } else {
+//   console.log(`Вы ввели не числовое значение попробуй еще раз`)
+// }
 
 // ===========Задача 4====================================
 
 //   let num1 = prompt (`Введите первое число для сравнения`)
 //   let num2 = prompt (`Введите второе число для сравнения`)
-
-  
-
 
 //   if ((!isNaN(Number(num1)) && num1 !== null && num1 !== '') && (!isNaN(Number(num2))&& num2 !== null && num2 !== '' )){
 //  if (num1>num2){
@@ -102,28 +93,26 @@
 
 // }
 
+// for(let i =60; i >=0; i--) {
 
-  // for(let i =60; i >=0; i--) {
+//   if (i === 30){
+//     console.log(`Ты на пол пути к 0`)
+//   } else [
+//     console.log(i)
+//   ]
 
-  //   if (i === 30){
-  //     console.log(`Ты на пол пути к 0`)
-  //   } else [
-  //     console.log(i)
-  //   ]
+// }
 
-  // } 
-
-  // let i = 60
-  // while (i >=0) {
-  //   if (i === 30){
-  //     i--
-  //     console.log(`Ты на пол пути к 0`)
-  //   } else {
-  //     console.log(i)
-  //   }
-  //   i--
-  // }
-
+// let i = 60
+// while (i >=0) {
+//   if (i === 30){
+//     i--
+//     console.log(`Ты на пол пути к 0`)
+//   } else {
+//     console.log(i)
+//   }
+//   i--
+// }
 
 // let balance
 
@@ -133,7 +122,7 @@
 //   }while ( balance)
 
 // let theme =  prompt(`Выбери тему`) // white // black //
- 
+
 // switch (theme) {
 //   case 'white':{
 //     console.log ('пользователь пожелал белую')
@@ -142,15 +131,13 @@
 //    case 'black':{
 //     console.log ('пользователь пожелал черную')
 //     break // выйти
-//   }; 
-    
+//   };
 
 //   default:
 //   {
 //     console.log ('пользователь ничего не выбрал')
 //   }
-  
-    
+
 // }
 
 // function logUserInfo(secondName , firstName, userAge, city ){ //локальная переменная
@@ -177,7 +164,7 @@
 //====================================================
 
 // функцию, которая принимает два аргумента (числовых значения) и выполняет с ними операцию, которую укажет user в качестве 3-го аргумента
- 
+
 // function logResult (onearg, twoarg, userInfo){
 //   let userInfo = onearg ( `Введите 1й аргумент`);
 //   let userInfo = twoarg ( `Введите 2й аргумент`);
@@ -188,10 +175,8 @@
 //      console.log (result);
 //   }
 
-  
 //   console.log ()
 // }
-
 
 // function controller (num1, num2, operator ) {
 // switch(operator){
@@ -210,7 +195,7 @@
 //     default:
 
 //     console.log('Введите (+, -, /, *)' )
-    
+
 // }
 // }
 // controller(8, 4, '*')
@@ -239,7 +224,7 @@
 //   const foo2 = (secondName) => {
 
 //     const foo3 = function ( age){
-     
+
 //       console.log(firstName,secondName,age)
 //     }
 //     foo3(age)
@@ -248,85 +233,93 @@
 // }
 
 // fool1('Vlad', 'Sobol', 32)
-// ===================================
+// ================================================================Объекты=============================================================================
 
 // let isShowPassword = false
 
-// let email = '321321'
-// const correctEmail = ' user@mail.ru';
-// const incorrectEmail = email;
-// let errorMessage = null;
+let email = "321321";
+const correctEmail = " user@mail.ru";
+const incorrectEmail = email;
+let errorMessage = null;
 
-// const validateEmail = function  (){
-//   console.log ("Первая функция foo")
-// }
-// //validateEmail()
-
-// const validateEmail2 = function foo(a){
-//   console.log ("Вторая функция foo")
-//   let c = a
-//   c++
+// const validateEmail = function () {
+//   console.log("Первая функция foo");
+// };
+// validateEmail();
+// _______________________________________________
+// const validateEmail2 = function foo(a) {
+//   console.log("Вторая функция foo");
+//   let c = a;
+//   c++;
 //   if (c === 10) return;
-  
-//   foo(1)
-// }
 
-//validateEmail2
+//   foo(1);
+// };
 
-// const validateEmail3 = ()=> correctEmail === email ? 'Входи' : 'Попробуй еще'
+// validateEmail2();
+// _______________________________________________
 
+// const validateEmail3 = () =>
+//   correctEmail === email ? "Входи" : "Попробуй еще";
+// console.log(validateEmail3());
 // ----------------------------------
-// const validateEmail3 = ()=> {
-//   console.log ("Стрелочная функция")
+// const validateEmail3 = () => {
+//   console.log("Стрелочная функция");
+// };
+// validateEmail3();
+// ____________________________________________
+
+// function validateEmail4() {
+//   console.log("Четвертая функция");
 // }
-// const validateEmail4() {
-//   console.log ("Четвертое функция")
-// }
+// validateEmail4();
+// _____________________________________________________________
 
-// const validateEmail = (userEmail)=> {
-// let = correctEmail === userEmail ? null : `Обязательно напишите ${correctEmail}`
-// // !!! null; Обязательно напишите user@mail.ru
-// console.log(error)
-
-
-// switch(error){  //null
+// const validateEmail = (userEmail) => {
+//   let =
+//     correctEmail === userEmail ? null : `Обязательно напишите ${correctEmail}`;
+//   // !!! null; Обязательно напишите user@mail.ru
+//   console.log(errorMessage);
+// };
+// validateEmail();
+// __________________________________
+// switch (
+//   error //null
+// ) {
 //   case `Обязательно напишите ${correctEmail}`:
 //     alert(error);
-//     break
-//     default:
-//       confirm ('Вы успешно ввошли. Продолжить')
+//     break;
+//   default:
+//     confirm("Вы успешно ввошли. Продолжить");
 // }
 
-// if  (!!error){
+// if (!!error) {
 //   alert(error);
-
-// }else {
-//   confirm ('Вы успешно ввошли. Продолжить')
+// } else {
+//   confirm("Вы успешно ввошли. Продолжить");
 // }
 
-// !!error? alert(error) : confirm ('Вы успешно ввошли. Продолжить') //3й вариант
+// !!error ? alert(error) : confirm("Вы успешно ввошли. Продолжить"); //3й вариант
 
 // }
-// // validateEmail(email)
-// validateEmail(correctEmail)
-
+// validateEmail(email)
+// validateEmail(correctEmail);
 
 // ================================================
 // switch (key) {
 //   case value:
-    
+
 //     break;
 
 //   default:
 //     break;
 // }
 
-
 // let d = 'global' //строка
 // const fool = (firstName,name,age) => {
 
 //     const foo2 = (secondName) => {
-  
+
 //       const foo3 = function ( age){
 //         let c = ('foo3')
 //         console.log(firstName,secondName,age)
@@ -335,53 +328,53 @@
 //     }
 //     foo2(secondName )
 //   }
-  
+
 //   fool1('Vlad', 'Sobol', 32)
 
 //  Объекты====================
-let str = 'Stroka'
-let str2 = ``
-let str3 = String ('')
+// let str = "Stroka";
+// let str2 = ``;
+// let str3 = String("");
 
-let name = " Tanya"
+// let name = " Tanya";
 
+// let user1 = {
+//   name: "Tanya",
+//   age: 35,
+//   hasJob: true,
+//   salary: 100,
+//   isMarried: true,
+//   emptyValye: null,
+//   emptyValye2: undefined,
+//   sayHello: function () {
+//     //функция - это метод
+//     console.log("  Привет, я ", user1.name);
+//     //  return  2
+//   },
+//   pet: {
+//     cat: {
+//       petName: "Tosya",
+//       age: 3,
+//     },
+//     dog: {
+//       petName: "Rex",
+//       age: 20,
+//     },
+//   },
+// };
 
-let user1 = {
-name: 'Tanya',
-age:35,
-hasJob: true,
-salary:100,
-isMarried: true,
-emptyValye: null,
-emptyValye2: undefined,
-sayHello: function (){ //функция - это метод
- console.log("  Привет, я ", user1.name);
-//  return  2
-},
-pet:{
-  cat:{
-    petName: 'Tosya',
-    age:3
-  },
-  dog:{
-    petName: 'Rex',
-    age:20,
-  }
-}
-}
+// console.log(user1.pet.cat.petName);
+// console.log(user1.pet.dog.petName);
+// console.log(user1.pet.dog.petName);
+// delete user1.emptyValye;
+// delete user1.emptyValye2;
+// console.log(user1);
 
-console.log(user1.pet.cat.petName);
-console.log(user1.pet.dog.petName);
-console.log(user1.pet.dog.petName);
-delete user1.emptyValye
-delete user1.emptyValye2
-console.log(user1)
-
-console.log(user1.age);// рекомендуется смотреть через точку
-console.log(user1); // при раскрытии новое значение
-user1.age = 25
-console.log(user1.age);
-console.log(user1);
+// console.log(user1.age); // рекомендуется смотреть через точку
+// console.log(user1); // при раскрытии новое значение
+// user1.age = 25;
+// console.log(user1.age);
+// console.log(user1);
 
 // let user2 = {
 // name: 'Dima',
@@ -409,9 +402,6 @@ console.log(user1);
 //  console.log(" Привет, мне ", user3.age);
 // }
 // }
-
-
-
 
 // console.log(user.name);
 // console.log(user.age);
